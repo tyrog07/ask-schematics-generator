c1d05abcc38847437efa4e82a7f6f826f87ad0db
Rajat Ninawe <rajat.ninawe@nitorinfotech.net>
2022-05-06 15:02:34 +0530

    bumped version to 1.0.5

--- src/ts-demo.ts
+++ src/ts-demo.ts
@@ -1,14 +1,13 @@
-import * as ts from 'typescript';
+import * as ts from "typescript";
 
 function showTree(node: ts.Node, depth: number = 0): void {
-    let indent = ''.padEnd(depth*4, ' ');
-    console.log(indent + ts.SyntaxKind[node.kind]);
-    if (node.getChildCount() === 0) {
-        console.log(indent + '    Text: ' + node.getText());
-    }
+  let indent = "".padEnd(depth * 4, " ");
+  console.log(indent + ts.SyntaxKind[node.kind]);
+  if (node.getChildCount() === 0) {
+    console.log(indent + "    Text: " + node.getText());
+  }
 
-    for(let child of node.getChildren()) {
-        showTree(child, depth+1);
-    }
+  for (let child of node.getChildren()) {
+    showTree(child, depth + 1);
+  }
 }
-
